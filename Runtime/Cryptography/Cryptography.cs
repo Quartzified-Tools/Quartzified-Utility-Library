e258462c1fa81cc9ec086ef45a03a10f394154df
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-07-01 13:16:00 +0200

    Added asmdef

--- Runtime/Cryptography/Cryptography.cs
+++ Runtime/Cryptography/Cryptography.cs
@@ -2,29 +2,32 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 
-public class Cryptography
+namespace Quartzified.Cryptography
 {
-	/// <summary>
-	/// Hashes the Input with use of a salt
-	/// </summary>
-	/// <param name="input">Input to Encrypt</param>
-	/// <param name="salt">Encryption Salt</param>
-	/// <returns></returns>
-	public static string PBKDF2Hash(string input, string salt)
+	public class Cryptography
 	{
-		byte[] bytes = Encoding.UTF8.GetBytes(salt);
-		return BitConverter.ToString(new Rfc2898DeriveBytes(input, bytes, 10000).GetBytes(20)).Replace("-", string.Empty);
-	}
+		/// <summary>
+		/// Hashes the Input with use of a salt
+		/// </summary>
+		/// <param name="input">Input to Encrypt</param>
+		/// <param name="salt">Encryption Salt</param>
+		/// <returns></returns>
+		public static string PBKDF2Hash(string input, string salt)
+		{
+			byte[] bytes = Encoding.UTF8.GetBytes(salt);
+			return BitConverter.ToString(new Rfc2898DeriveBytes(input, bytes, 10000).GetBytes(20)).Replace("-", string.Empty);
+		}
 
-	/// <summary>
-	/// Hashes the Input with use of a salt
-	/// </summary>
-	/// <param name="input">Input to Encrypt</param>
-	/// <param name="salt">Encryption Salt</param>
-	/// <returns></returns>
-	public static string PBKDF2Hash(string input, int salt)
-	{
-		byte[] bytes = BitConverter.GetBytes(salt);
-		return BitConverter.ToString(new Rfc2898DeriveBytes(input, bytes, 10000).GetBytes(20)).Replace("-", string.Empty);
+		/// <summary>
+		/// Hashes the Input with use of a salt
+		/// </summary>
+		/// <param name="input">Input to Encrypt</param>
+		/// <param name="salt">Encryption Salt</param>
+		/// <returns></returns>
+		public static string PBKDF2Hash(string input, int salt)
+		{
+			byte[] bytes = BitConverter.GetBytes(salt);
+			return BitConverter.ToString(new Rfc2898DeriveBytes(input, bytes, 10000).GetBytes(20)).Replace("-", string.Empty);
+		}
 	}
 }
