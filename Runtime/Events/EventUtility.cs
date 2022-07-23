20709fcf243968ecbc77131fbec81a1e5ff04dc8
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-07-23 14:50:14 +0200

    Added new Careful Deselect

--- Runtime/Events/EventUtility.cs
+++ Runtime/Events/EventUtility.cs
@@ -1,4 +1,10 @@
+using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
 
 namespace Quartzified.Events
 {
@@ -14,6 +20,29 @@ namespace Quartzified.Events
         {
             uEvent.RemoveAllListeners();
             uEvent.AddListener(call);
+        }
+
+        public static void DeselectCarefully()
+        {
+#if ENABLE_INPUT_SYSTEM
+            Mouse mouse = Mouse.current;
+            if (mouse != null)
+            {
+                if (!mouse.leftButton.wasPressedThisFrame &&
+                    !mouse.rightButton.wasPressedThisFrame &&
+                    !mouse.middleButton.wasPressedThisFrame)
+                {
+                    EventSystem.current.SetSelectedGameObject(null);
+                }
+            }
+#elif ENABLE_LEGACY_INPUT_MANAGER
+            if (!Input.GetMouseButton(0) &&
+                !Input.GetMouseButton(1) &&
+                !Input.GetMouseButton(2))
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+#endif
         }
     }
 }
